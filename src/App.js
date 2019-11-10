6cbc1f048d677235d67e0ad9142cc101535bb530
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2019-11-10 08:06:47 -0300

    Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
 
 class App extends Component {
+  state = {
+    noticias: []
+  };
+
+  componentDidMount() {
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=915942ad794843e8af4c132463c0da58`;
+
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+
+    this.setState({ noticias: noticias.articles });
+  };
+
   render() {
-    return <div>Noticias API React</div>;
+    return (
+      <Fragment>
+        <Header titulo="Noticias React API" />
+        <div className="container white contenedor-noticias"></div>
+      </Fragment>
+    );
   }
 }
 
